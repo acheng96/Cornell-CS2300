5d13914d0e2eb1ab81c05e62943e02de76ce321e
Annie Cheng <ac962@cornell.edu>
2016-02-08 14:34:44 -0500

    Finish styling for contact page

--- p1_cs2300/files/contact.php
+++ p1_cs2300/files/contact.php
@@ -8,33 +8,39 @@
 		<title>Contact Page</title>
 	</head>
 	<body>
-
 		<!-- Header -->
 		<?php include("header.php"); ?>
 
 		<!-- Body -->
 		<div id="contact-body-container">
-			<div>
-				<img id="potato" src="../images/potato.png" alt="Potato">
-				<p id="fun-fact">Fun Fact: I like potatoes</p>
+			<div class="fun-fact-section">
+				<h1 id="fun-fact">Fun Fact: I love potatoes.</h1>
+				<img id="potato" src="../images/hot_potato.png" alt="Potato">
+				<p id="fun-fact-question">What's your favorite type of potato?</p>
+				<select name="potatoes">
+					<option value="0">Hot</option>
+					<option value="1">Baked</option>
+					<option value="2">French Fries</option>
+					<option value="3">Hashbrown</option>
+					<option value="4">Mashed</option>
+				</select>
 			</div>
 
 			<!-- Contact Form -->
-			<div class="contact-form">
-				<div></div>
-				<div id="about-hdivider"></div>
-				<form action="">
-				  <input type="text" name="Name"><br>
-				  <input type="text" name="Email"><br>
-				  <input type="text" name="Subject"><br>
-				  <input type="text" name="Message"><br>
+			<div class="contact-form-container">
+				<h1>SAY HELLO</h1>
+				<div id="contact-hdivider"></div>
+				<form class="contact-form" action="">
+				  <input type="text" name="Name" placeholder="NAME"><br>
+				  <input type="text" name="Email" placeholder="EMAIL"><br>
+				  <input type="text" name="Subject" placeholder="SUBJECT"><br>
+				  <textarea name="Message" placeholder="MESSAGE"></textarea><br>
 				  <input type="submit" value="Submit">
 				</form>
 			</div>
 		</div>
 
-		<!-- Footer -->
-		<?php include("footer.php"); ?>
-
+	    <!-- Footer -->
+	    <?php include("footer.php"); ?>
 	</body>
 </html>
