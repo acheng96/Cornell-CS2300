7c6a9809b4bacdb3f41900405ce1519fe2d94e96
Annie Cheng <ac962@cornell.edu>
2016-03-21 19:23:28 -0400

    Add integer validation for album id

--- p3/index.php
+++ p3/index.php
@@ -64,8 +64,15 @@
 			$albums = array();
 			$photos = array();
 
-			// Set album_id and album_title when album clicked
-			if (isset($_GET['album_id'])) {
+			// Retrieve albums
+			$albumsResult = $mysqli->query("SELECT * FROM Albums");
+
+			while ($row = $albumsResult->fetch_row()) {
+				$albums[] = new Album($row[0], $row[1], $row[2], $row[3], $row[4], $row[5]);
+			}
+
+			// Set album_id and album_title when album clicked; check if album id is a valid number
+			if (isset($_GET['album_id']) && ctype_digit($_GET['album_id'])) {
 				$album_id = $_GET['album_id'];
 				$album = $mysqli->query(
 					"SELECT * FROM Albums 
@@ -74,27 +81,20 @@
 
 				$albumRow = $album->fetch_row();
 				$album_title = strtoupper($albumRow[1]);
-			}
 
-			// Retrieve albums
-			$albumsResult = $mysqli->query("SELECT * FROM Albums");
+				// Retrieve photos from album with selected album_id
+				$photosResult = $mysqli->query(
+					"SELECT * FROM Photos 
+					INNER JOIN PhotoInAlbum 
+					ON Photos.photo_id = PhotoInAlbum.photo_id
+					INNER JOIN Albums
+					ON PhotoInAlbum.album_id = Albums.album_id
+					WHERE PhotoInAlbum.album_id = $album_id"
+				);
 
-			// Retrieve photos from album with selected album_id
-			$photosResult = $mysqli->query(
-				"SELECT * FROM Photos 
-				INNER JOIN PhotoInAlbum 
-				ON Photos.photo_id = PhotoInAlbum.photo_id
-				INNER JOIN Albums
-				ON PhotoInAlbum.album_id = Albums.album_id
-				WHERE PhotoInAlbum.album_id = $album_id"
-			);
-
-			while ($row = $albumsResult->fetch_row()) {
-				$albums[] = new Album($row[0], $row[1], $row[2], $row[3], $row[4], $row[5]);
-			}
-
-			while ($row = $photosResult->fetch_row()) {
-				$photos[] = new Photo($row[0], $row[1], $row[2], $row[3], $row[4], $row[5]);
+				while ($row = $photosResult->fetch_row()) {
+					$photos[] = new Photo($row[0], $row[1], $row[2], $row[3], $row[4], $row[5]);
+				}
 			}
 
 			$mysqli -> close();
@@ -127,28 +127,33 @@
 		<!-- Display albums or photos -->
 		<?php 
 			if (isset($_GET['album_id'])) {
-			    # If there is a GET argument for 'id', we have some code to handle it (display an album whose albumID matches $_GET['id']
+			    # Display photos in album with album_id = $album_id
 			    $album_id = $_GET['album_id'];
 
-			    print "<h3 id='photos-title'>ALBUM #{$album_id}: {$album_title}</h3>
-			    <a href='index.php'><h3 id='back-button'>RETURN TO ALBUMS</h3></a>
-				<div class='photos'>
-					<div class='photos-container'>";
-						for ($i = 0; $i < count($photos); $i++) { 
-							$imageName = $photos[$i]->photoName;
-							$altName = str_replace(' ', '', $imageName);
-	
-						print "<div class='photo-item'>
-								<img class='photo-image' src='{$photos[$i]->photoFilePath}'  alt='{$altName}'>
-								<p class='photo-title'>#{$photos[$i]->photoId}: {$photos[$i]->photoName}</p>
-								<p class='photo-caption'>{$photos[$i]->photoCaption}</p>
-								<h4 class='photo-credit'>Image from <a href='{$photos[$i]->photoCredit}' target='_blank'><b>here</b></a>.</h4>
-							</div>";
-						}
-					print "</div>
-				</div>";
+			    if (!ctype_digit($album_id) || $album_title == "") { // Display error message if album id is not a valid id
+				    print "<h3 id='album-id-error'>OH NO! THIS ALBUM DOESN'T EXIST!</h3>
+				    <a href='index.php'><h3 id='back-button'>RETURN TO ALBUMS</h3></a>";
+				} else {
+					print "<h3 id='photos-title'>ALBUM #{$album_id}: {$album_title}</h3>
+				    <a href='index.php'><h3 id='back-button'>RETURN TO ALBUMS</h3></a>
+					<div class='photos'>
+						<div class='photos-container'>";
+							for ($i = 0; $i < count($photos); $i++) { 
+								$imageName = $photos[$i]->photoName;
+								$altName = str_replace(' ', '', $imageName);
+		
+							print "<div class='photo-item'>
+									<img class='photo-image' src='{$photos[$i]->photoFilePath}'  alt='{$altName}'>
+									<p class='photo-title'>#{$photos[$i]->photoId}: {$photos[$i]->photoName}</p>
+									<p class='photo-caption'>{$photos[$i]->photoCaption}</p>
+									<h4 class='photo-credit'>Image from <a href='{$photos[$i]->photoCredit}' target='_blank'><b>here</b></a>.</h4>
+								</div>";
+							}
+						print "</div>
+					</div>";
+				}
 			} else {
-			    # There isn't a ?id argument, so we want to display something else (if it's albums.php, we'll probably just display all our albums here)
+				# Display all albums
 			    print "<p id='home-description'>Welcome to the Worldwide Wonders Photo Gallery! Here, you can find your next bucket list place to visit!</p>";
 
 			    print "<div class='album-container'>";
@@ -157,7 +162,7 @@
 						<h4 class='album-date-created'>DATE CREATED: {$albums[$i]->albumDateCreated}</h4>
 						<h4 class='album-date-modified'>DATE MODIFIED: {$albums[$i]->albumDateModified}</h4>
 						<h4 class='image-credit'>Image from <a href='{$albums[$i]->albumPhotoCredit}' target='_blank'><b>here</b></a>.</h4>
-						<a href='index.php?album_id={$albums[$i]->albumId}''><img class='album-image' src='{$albums[$i]->albumPhotoFilePath}'  alt='Album'></a>";
+						<a href='index.php?album_id={$albums[$i]->albumId}'><img class='album-image' src='{$albums[$i]->albumPhotoFilePath}'  alt='Album'></a>";
 					}	
 				print "</div>";
 			}
